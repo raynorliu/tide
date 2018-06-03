ec4fd001db0d10f5dc4366a21ded012d8b855a7b
156572553@qq.com <156572553@qq.com>
2018-06-03 16:33:08 +0800

    # WARNING: head commit changed in the meantime
    添加log4j日志系统

--- src/main/java/com/ome/algorithm/BubbleSort.java
+++ src/main/java/com/ome/algorithm/BubbleSort.java
@@ -9,11 +9,16 @@ import java.util.Arrays;
  *
  */
 public class BubbleSort {
-	private static int[] arr = { 5, 4, 7, 9, 8, 3, 10, 6, 1, 2 };
+	// private static int[] arr = { 5, 4, 7, 9, 8, 3, 10, 6, 1, 2 };
+	private static int[] arr = { 5, 4, 7, 9, 8, 3, 9, 10, 11, 12 };
 
 	/**
 	 * 第一种的冒泡排序算法<br>
 	 * 总循环次数为54次
+	 * 
+	 * 比如，现在有一个包含1000个数的数组，仅前面100个无序，后面900个都已排好序且都大于前面100个数字，
+	 * 那么在第一趟遍历后，最后发生交换的位置必定小于100，且这个位置之后的数据必定已经有序了，也就是这个位置以后的数据不需要再排序了，
+	 * 于是记录下这位置，第二次只要从数组头部遍历到这个位置就可以了。
 	 * 
 	 * @param arr
 	 */
@@ -23,17 +28,18 @@ public class BubbleSort {
 		int flag = arr.length;
 		while (flag > 0) {
 			count += 1;
-			k = flag;
+			k = flag; // 记录遍历的尾边界
 			flag = 0;// 设置while退出条件
 
 			for (int i = 1; i < k; i++) {
 				count += 1;
 				if (arr[i - 1] > arr[i]) {
+					System.out.println(i);
 					int temp;
 					temp = arr[i - 1];
 					arr[i - 1] = arr[i];
 					arr[i] = temp;
-					flag = i;
+					flag = i;// 记录最新的尾边界
 				}
 			}
 		}
@@ -144,6 +150,94 @@ public class BubbleSort {
 		System.out.println("排序后的数组元素arr = " + Arrays.toString(arr));
 	}
 
+	/**
+	 * 求四分位距
+	 * 
+	 * @param arr
+	 */
+	public float numerationIQR(int[] arr) {
+		float Q2;// 中位数
+		float Q1;// 下四分位数
+		float Q3;// 上四分位数
+		float IQR = 0; // 四分位距
+		int j;
+		int[] arrQ1;// 存放下四分位的数列
+		int[] arrQ3;// 存放上四分位的数列
+
+		// 对列表排序
+		Arrays.sort(arr);
+		// 计算四分位距
+		if (arr.length % 2 == 0) {// 列表为总数量为偶数
+			System.out.println("进入偶数列表算法");
+			j = arr.length / 2;
+
+			// 计算下中位数
+			Q2 = (arr[j - 1] + (arr[j])) / 2;
+			System.out.println("中位数Q2= " + Q2);
+
+			// 判断下分位距的元素列表的数量是基数还是偶数
+			arrQ1 = Arrays.copyOfRange(arr, 0, j);
+			arrQ3 = Arrays.copyOfRange(arr, j, arr.length);
+			System.out.println(Arrays.toString(arrQ1));
+			System.out.println(Arrays.toString(arrQ3));
+
+			if (arrQ1.length % 2 == 0) {
+				// 计算下四分位数
+				Q1 = (float) (arrQ1[arrQ1.length / 2] + arrQ1[(arrQ1.length / 2) - 1]) / 2;
+				System.out.println("下四分位数Q1 = " + Q1);
+
+				// 计算上四分位数
+
+				Q3 = (float) (arrQ3[arrQ3.length / 2] + arrQ3[(arrQ3.length / 2) - 1]) / 2;
+				System.out.println("上四分位数Q3 = " + Q3);
+			} else {
+				// 计算下四分位数
+				Q1 = arrQ1[(arrQ1.length - 1) / 2];
+				System.out.println("下四分位数Q1 = " + Q1);
+
+				// 计算上四分位数
+				Q3 = arrQ3[(arrQ1.length - 1) / 2];
+				System.out.println("上四分位数Q3 = " + Q3);
+
+			}
+			// 计算四分位距
+			IQR = Q3 - Q1;
+			System.out.println("四分位距IQR = Q3 - Q1 = " + Q3 + " - " + Q1 + " = " + IQR);
+		} else {
+			System.out.println("进入奇数列表算法");
+			j = (arr.length - 1) / 2;
+			// 计算下中位数
+			Q2 = arr[j];
+			System.out.println("中位数Q2= " + Q2);
+
+			// 判断下分位距的元素列表的数量是基数还是偶数
+			arrQ1 = Arrays.copyOfRange(arr, 0, j);
+			arrQ3 = Arrays.copyOfRange(arr, j + 1, arr.length);
+			System.out.println(Arrays.toString(arrQ1));
+			System.out.println(Arrays.toString(arrQ3));
+			if (arrQ1.length % 2 == 0) {
+				// 计算下四分位数
+				Q1 = (float) (arrQ1[(arrQ1.length / 2) - 1] + arrQ1[(arrQ1.length / 2)]) / 2;
+				System.out.println("下四分位数Q1 = " + Q1);
+
+				// 计算上四分位数
+				Q3 = (float) (arrQ3[(arrQ3.length / 2) - 1] + arrQ3[arrQ3.length / 2]) / 2;
+				System.out.println("上四分位数Q3 = " + Q3);
+			} else {
+				Q1 = arrQ1[(arrQ1.length - 1) / 2];
+				System.out.println("下四分位数Q1 = " + Q1);
+
+				// 计算上四分位数
+				Q3 = arrQ3[(arrQ3.length - 1) / 2];
+				System.out.println("上四分位数Q3 = " + Q3);
+			}
+			// 计算四分位距
+			IQR = Q3 - Q1;
+			System.out.println("四分位距IQR = Q3 - Q1 = " + Q3 + " - " + Q1 + " = " + IQR);
+		}
+		return IQR;
+	}
+
 	public static void main(String[] args) {
 		BubbleSort bubbleSort = new BubbleSort();
 		// bubbleSort.firstAlgorithm(arr);
@@ -151,5 +245,11 @@ public class BubbleSort {
 		// bubbleSort.thirdAlgorithm(arr);
 		// bubbleSort.fourthlyAlgorithm(arr);
 		// bubbleSort.fifthAlgorithm(arr);
+		// int[] list = { 7, 9, 9, 10, 10, 10, 11, 12, 12, 14 };
+		// int[] list = { 4, 5, 6, 7, 10, 11, 12, 14, 15 };
+		// int[] list = { 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15 };
+		// int[] list = { 3, 4, 4, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+		 int[] list = { 3, 4, 4, 5, 5, 5, 6, 7 };
+		 bubbleSort.numerationIQR(list);
 	}
 }
